2e031d100e8f6884d6393287b0646cb852ecde1e
YashRTech <ramjitiwari4455@gmail.com>
2025-04-21 19:57:22 +0530

    Day 1(Basics) updated

---
+++ Day 1(Basics)/script.js
@@ -0,0 +1,14 @@
+const form = document.getElementById("example-form");
+const a = form.elements["a"];
+const b = form.elements["b"];
+const result = form.elements["result"];
+
+function updateResult() {
+  const aValue = parseInt(a.value);
+  const bValue = parseInt(b.value);
+  result.value = aValue + bValue;
+}
+
+form.addEventListener("input", updateResult);
+
+updateResult();
