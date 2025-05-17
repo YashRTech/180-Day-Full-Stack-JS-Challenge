4433aa228702f87086bba9b641936b6a10a219c1
Yash Tiwari <ramjitiwari4455@gmail.com>
2025-05-17 05:13:04 +0000

    Day 23 updated

---
+++ Day 23 - ES6/ES6/one.js
@@ -0,0 +1,6 @@
+const greeting = "Hello, Odinite!";
+const farewell = "Bye bye Odinite";
+
+export default "I am Gamer"
+
+export { greeting, farewell };
---
+++ Day 23 - ES6/ES6/two.js
@@ -0,0 +1,5 @@
+import gamer , { greeting, farewell } from "./one.js";
+
+console.log(greeting)
+console.log(farewell)
+console.log(gamer)
